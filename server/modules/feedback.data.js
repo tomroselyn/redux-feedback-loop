478029663f1938e664ea308562c6a6b604c0dc6c
Thomas Roselyn <tgroselyn@gmail.com>
2019-05-11 09:56:17 -0500

    server setup started

---
+++ server/modules/feedback.data.js
@@ -0,0 +1,10 @@
+let feedbackData = [
+    {
+        feeling: 3,
+        understanding: 3,
+        support: 3,
+        comments: `third time's a charm`
+    }
+]
+
+module.exports = feedbackData;
---
+++ server/routes/feedback.router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+let feedbackData = require('../modules/feedback.data.js');
+
+//GET
+router.get('/', (req, res) => {
+    res.send(feedbackData);
+}).catch(error => {
+    res.sendStatus(500);
+}); //end GET
+
+//POST
+router.post('/', (req, res) => {
+    feedbackData.push(req.body);
+    res.sendStatus(201);
+}).catch(error => {
+    res.sendStatus(500);
+}); //end POST
